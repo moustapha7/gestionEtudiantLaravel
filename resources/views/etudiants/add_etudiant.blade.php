9b586f47d06fdf793612e9563a2f6e9671436224
moustapha <moustaphadiedhiou7@gmail.com>
2019-02-03 14:44:11 +0000

    fin du projet

--- resources/views/etudiants/add_etudiant.blade.php
+++ resources/views/etudiants/add_etudiant.blade.php
@@ -43,7 +43,7 @@
         <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
       </div>
       <!--logo start-->
-      <a href="index.html" class="logo"><b>DASH<span>IO</span></b></a>
+      <a href="index.html" class="logo"><b>Gestion <span>Etudiants</span></b></a>
       <!--logo end-->
       <div class="nav notify-row" id="top_menu">
         <!--  notification start -->
